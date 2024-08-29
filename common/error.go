35c873f6c46ebecc1003c47ba818464b05f7a35c
Abhijana <abhijanaramanda@gmail.com>
2024-08-30 02:14:10 +0800

    feat: define http error factory, standard response body schema

---
+++ common/error.go
@@ -0,0 +1,25 @@
+package common
+
+import "net/http"
+
+type Error struct {
+	Code    int
+	Message string
+	Cause   error
+}
+
+func newError(code int, msg string, cause error) *Error {
+	return &Error{code, msg, cause}
+}
+
+func (e *Error) Error() string {
+	return e.Cause.Error()
+}
+
+func NewBadRequestError(msg string, cause error) *Error {
+	return newError(http.StatusBadRequest, msg, cause)
+}
+
+func NewInternalError(cause error) *Error {
+	return newError(http.StatusInternalServerError, "Kesalahan internal server", cause)
+}
--- common/response.go
+++ common/response.go
@@ -3,3 +3,36 @@ package common
 type CommonResponse struct {
 	Data interface{}
 }
+
+type BaseResponse[T any] struct {
+	Data    T           `json:"data,omitempty"`
+	Message string      `json:"message"`
+	Meta    interface{} `json:"meta,omitempty"`
+	Cause   interface{} `json:"cause,omitempty"`
+	Status  bool        `json:"status"`
+}
+
+type PageMetaResponse struct {
+	Current      int   `json:"current"`
+	TotalPages   int   `json:"totalPages"`
+	PerPage      int   `json:"perPage"`
+	TotalRecords int64 `json:"totalRecords"`
+}
+
+func NewBaseResponse[T interface{}](message string, data T, meta interface{}) *BaseResponse[T] {
+	return &BaseResponse[T]{
+		Message: message,
+		Data:    data,
+		Meta:    meta,
+		Status:  true,
+	}
+}
+
+func NewErrorResponse(message string, cause interface{}) *BaseResponse[any] {
+	return &BaseResponse[any]{
+		Message: message,
+		Data:    nil,
+		Status:  false,
+		Cause:   cause,
+	}
+}
