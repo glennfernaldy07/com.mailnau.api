506f95bd8c2a092145412523fd5d1304b2082c72
Abhijana <abhijanaramanda@gmail.com>
2024-08-30 17:24:51 +0800

    feat: entity models for get roles list

--- role-menu-action/domain/model.go
+++ role-menu-action/domain/model.go
@@ -6,6 +6,21 @@ type RoleMenuAction struct {
 	ActionID int64 `gorm:"column:action_id;foreignKey"`
 }
 
+type Menu struct {
+	ID   int64  `gorm:"primaryKey"`
+	Name string `gorm:"size:50;not null"`
+}
+
+type Action struct {
+	ID   int64  `gorm:"primaryKey"`
+	Name string `gorm:"size:50;not null"`
+}
+
 func (RoleMenuAction) TableName() string {
 	return "role_menu_action"
+}
+
+type MenuOrActionDTO struct {
+	ID   int64  `json:"id"`
+	Name string `json:"name"`
 }
--- role/domain/model.go
+++ role/domain/model.go
@@ -1,13 +1,19 @@
 package domain
 
-import "time"
+import (
+	"time"
+
+	rma_domain "com.mailnau.api/role-menu-action/domain"
+)
 
 type Role struct {
-	ID        int64     `gorm:"column:id;primaryKey"`
-	Name      string    `gorm:"column:name"`
-	Echelon   string    `gorm:"column:echelon"`
-	CreatedAt time.Time `gorm:"column:created_at"`
-	UpdatedAt time.Time `gorm:"column:updated_at"`
+	ID        int64               `gorm:"column:id;primaryKey"`
+	Name      string              `gorm:"column:name"`
+	Echelon   string              `gorm:"column:echelon"`
+	Menus     []rma_domain.Menu   `gorm:"many2many:role_menu_action;joinForeignKey:RoleID;joinReferences:MenuID"`
+	Actions   []rma_domain.Action `gorm:"many2many:role_menu_action;joinForeignKey:RoleID;joinReferences:ActionID"`
+	CreatedAt time.Time           `gorm:"column:created_at"`
+	UpdatedAt time.Time           `gorm:"column:updated_at"`
 }
 
 func (Role) TableName() string {
@@ -16,4 +22,12 @@ func (Role) TableName() string {
 
 type CreateRoleResponse struct {
 	RoleId int64 `json:"roleId"`
+}
+
+type RoleDTO struct {
+	ID       int64                        `json:"id"`
+	RoleName string                       `json:"roleName"`
+	Echelon  string                       `json:"echelon"`
+	Menus    []rma_domain.MenuOrActionDTO `json:"menus"`
+	Actions  []rma_domain.MenuOrActionDTO `json:"actions"`
 }
